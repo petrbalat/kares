e2b1e9cbc42248dedf0a42c04ff35e3a2d6d0007
Petr Balat <balat@oksystem.cz>
2017-11-13 14:51:22 +0100

    0.2.1 - google merchants scheme

---
+++ src/main/java/cz/petrbalat/xml/google/merchants/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema(xmlns = {@XmlNs(prefix = "g", namespaceURI = "http://base.google.com/ns/1.0")})
+package cz.petrbalat.xml.google.merchants;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
---
+++ src/main/kotlin/cz/petrbalat/xml/google/merchants/merchants.kt
@@ -0,0 +1,7 @@
+package cz.petrbalat.xml.google.merchants
+
+/**
+ *
+ * @author balat
+ */
+const val MERCHANTS_SCHEMA = "http://base.google.com/ns/1.0"
